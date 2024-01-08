67576a6df960927af3f7dbf3ce693403dd9b624e
Artem Erusov <erusovav@gmail.com>
2024-01-08 14:55:07 +0300

    Added an array with products

--- src/pages/mainPages/components/sectionBestSellers/components/CardCategory.jsx
+++ src/pages/mainPages/components/sectionBestSellers/components/CardCategory.jsx
@@ -1,5 +1,5 @@
 import styles from './styles.module.scss';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 const CardCategory = () =>  {
 
@@ -7,21 +7,17 @@ const CardCategory = () =>  {
 
   const bestSellersCategory = ['Все платформы', 'PC', 'PS 4', 'PS 5', 'Xbox', 'Switch']
 
-  return<Fragment>
-    <div>
+  return(
       <ul className={styles.select}>
       {bestSellersCategory.map((item, index) =>
-        <li 
-          onClick={() => setsSelectCategory(index)} 
-          key={index} 
+        <li onClick={() => setsSelectCategory(index)}  key={index} 
           className={ selectCategory === index ? (styles.select__item + ' ' + styles.select__active) : styles.select__item}>
           {item}
         </li>
       )
       }
       </ul>
-    </div>
-  </Fragment>
+  );
 };
 
 export default CardCategory;
