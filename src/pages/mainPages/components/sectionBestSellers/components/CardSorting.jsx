379d81d0e85ac9a91e3f67be7dae2abbf243ecda
Artem Erusov <erusovav@gmail.com>
2024-01-08 18:43:20 +0300

    started doing CardSorting

--- src/pages/mainPages/components/sectionBestSellers/components/CardSorting.jsx
+++ src/pages/mainPages/components/sectionBestSellers/components/CardSorting.jsx
@@ -1,10 +1,33 @@
 import styles from './styles.module.scss';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 const CardSorting = () =>  {
-  return<Fragment>
-    <div className={styles.category}>
 
+  const sortiСategories = ['Популярности', 'Цене', 'Рейтингу'];
+  const [selectSortCategories, setSelectSortCategories] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return<Fragment>
+    <div className={styles.sort}>
+      <p className={styles.sort__p}>Сортировка по:</p>
+      <p onClick={()=>setIsOpen(!isOpen)} className={styles.sort__p_active}>{sortiСategories[selectSortCategories]}</p>
+
+      { isOpen && (
+            <ul className={styles.sort__drop}>
+            {sortiСategories.map((item, index) =>
+              <li
+              key = {index}
+              onClick={() => setSelectSortCategories(index)}
+              className={sortiСategories === index ? sort__active : ''}>
+                {item}
+              </li>
+            )}
+            </ul>
+      )}
+
+      
+     
+     
     </div>
   </Fragment>
 };
